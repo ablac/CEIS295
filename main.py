49875d19e7066f1881f52aa7507fc2d5fb163600
Keith Swoger <ablac.nezgolur@gmail.com>
2022-10-29 16:41:58 +0000

    Updated Menu Created Start Function Added Clear Function

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
-# User Menu
-print ("Insert 1 for TimeProcess")
-print ("Insert 2 for ArrayList")
-print ("Insert 3 for QuickSort")
-print ("Insert E to exit")
+import os
 
-# Request Input
-select = input("Select Process: ")
+def start():
+  # User Menu
+  print ("Insert 1 for TimeProcess")
+  print ("Insert 2 for ArrayList")
+  print ("Insert 3 for QuickSort")
+  print ("Insert C to clear console")
+  print ("Insert E to exit")
+  
+  # Request Input
+  select = input("Select Process: ")
+  
+  # Convert to uppercase
+  select = select.upper()
+  
+  # Run Selected Process
+  if select == "1":
+    import TimeProcess
+  elif select == "2":
+    import ArrayList
+  elif select == "3":
+    import QuickSort
 
-# Run Time Process
-if select == "1":
-  print ("Test")
-  import TimeProcess
+  # Clear Console.
+  elif select == "C":
+    os.system('clear')  
+    start()
+    
+  # Exit Code
+  elif select == "E":
+    quit
+    
+  # Invalid Entry  
+  else:
+    print("Invalid Entry")
+    start()
 
-# Exit Code
-if select == "E":
-  quit
+# Run Program.    
+start()
+    
+  
